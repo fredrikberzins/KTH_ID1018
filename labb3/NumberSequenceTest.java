18d5aec4a005559751315898033db2c08fd35e58
fredrikberzins <fredrik.berzins@gmail.com>
2022-11-10 01:18:04 +0100

    fixed value

--- labb3/NumberSequenceTest.java
+++ labb3/NumberSequenceTest.java
@@ -19,8 +19,8 @@ class NumberSequenceTest
 		double[] realNumbers =
 		    {1.0, 2.0, 3.0, 5.0, 8.0, 13.0, 21.0};
         NumberSequence sequence = null;
-        sequence = new ArrayNumberSequence(realNumbers);
-        //sequence = new LinkedNumberSequence(realNumbers);
+        //sequence = new ArrayNumberSequence(realNumbers);
+        sequence = new LinkedNumberSequence(realNumbers);
         out.println("the sequence:");
         out.println(sequence);
         out.println();
@@ -31,7 +31,7 @@ class NumberSequenceTest
         out.println("one lower bound: " + sequence.lowerBound());
         out.println();
 
-        int positionPlace = 10;
+        int positionPlace = 4;
         double positionValue = 8;
         try {out.println("number at position " + positionPlace + ": " +  sequence.numberAt(positionPlace));
         } catch (IndexOutOfBoundsException newException) {
